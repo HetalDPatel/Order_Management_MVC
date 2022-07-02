5ab305999e8d932a5f53776ceb7c2876e21b5fbe
HetalDPatel <hetalgohil29@gmail.com>
2022-07-02 14:15:22 -0400

    Adding Database file

---
+++ Db_MVC.sql
@@ -0,0 +1,52 @@
+USE [master]
+GO
+-- drop database
+Drop database MVC
+GO
+-- create the database
+CREATE DATABASE MVC
+GO
+
+--switch to MVC
+Use MVC
+GO
+
+-- Create Table for orders
+Create table orders
+	(order_id int IDENTITY(1,1) PRIMARY KEY nonClustered,
+	order_no int not null,
+	order_date datetime not null,	
+	item nvarchar(50)not null,
+	qty int not null,
+	price_per_item money not null,	
+	customer_name nvarchar(50) not null,
+	address_id int not null)
+	Go
+-- Insert Data into orders
+Insert into orders values(10000,'06/29/2022','Dell intel Core i7 laptop',2,2008.97,'James Arthur',1);
+Insert into orders values(10000,'06/29/2022','Dell intel Core i7 laptop',2,2008.97,'James Arthur',1);
+--select
+
+select * from orders;
+
+Create table address
+(
+	address_id int primary key nonClustered,
+	house_no nvarchar(50),
+	street nvarchar(50),
+	city nvarchar(30),
+	country nvarchar(30),
+	postal_code nvarchar(10)
+)
+Go
+
+Insert into address values(1,'504','2000 Bay street','Hamilton','Canada','L8P8C4');
+
+select * from address;
+
+
+select o.order_no as 'Order No',o.order_date as 'Order Date',o.item as 'Item Description',
+	o.qty as Quantity ,Concat(o.price_per_item,'$')as Cost ,o.customer_name as 'Customer Name',
+	CONCAT('#',a.house_no,', ',a.street) as Address,a.city as City ,a.postal_code as Postal,a.country as Country from orders o
+join address a
+on o.address_id=a.address_id;
