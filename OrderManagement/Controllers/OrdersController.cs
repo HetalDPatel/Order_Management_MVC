14a04eade2cf83d9aa0651da015a426f9bbdca7a
HetalDPatel <hetalgohil29@gmail.com>
2022-07-04 16:44:22 -0400

    Order controller added again

---
+++ OrderManagement/Controllers/OrdersController.cs
@@ -0,0 +1,99 @@
+﻿using OrderManagement.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderManagement.Controllers
+{
+    public class OrdersController : Controller
+    {
+        Order order = new Order();
+        // GET: Orders
+        public ActionResult Index()
+        {
+           
+
+            //Fetching data from database
+            var orderList = order.GetOrders().ToList();
+
+            // var ord = order.GetOrder(1).Map();
+
+            return View(orderList);
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int id)
+        {
+            var ord = order.GetOrder(id);
+            return View(ord);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Orders/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
