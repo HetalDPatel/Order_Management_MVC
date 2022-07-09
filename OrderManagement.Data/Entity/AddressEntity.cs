8bb20edd0ace2437ac6f1b67f8b53b3c32d2be06
Kishan153 <62192079+Kishan153@users.noreply.github.com>
2022-07-08 22:23:37 -0400

    create new order function added

--- OrderManagement.Data/Entity/AddressEntity.cs
+++ OrderManagement.Data/Entity/AddressEntity.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace OrderManagement.Data.Entity
 {
     public class AddressEntity
     {
         public int AddressId { get; set; }
+        public string HouseNo { get; set; }
         public string Street { get; set; }
         public string Country { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
 
         public virtual IEnumerable<OrderEntity> OrderList { get; set; }
     }
