e88f1f351ad9ca310c1669c6f5fd0a7cd416e0c6
HetalDPatel <hetalgohil29@gmail.com>
2022-07-14 17:46:14 -0400

    Dropdownlist added using address id on CreateOrder Page

--- OrderManagement.Data/Order.cs
+++ OrderManagement.Data/Order.cs
@@ -30,12 +30,12 @@ namespace OrderManagement.Data
         public List<OrderEntity> GetOrders()
         {
 
-            
+
             string query = $"select o.order_id,o.order_no as 'Order No',o.order_date as 'Order Date',o.item as 'Item Description',o.qty as Quantity ," +
                 $"o.price_per_item as Cost ,o.customer_name as 'Customer Name'," +
                 $"CONCAT('#', a.house_no, ', ', a.street, ', ', a.city, '-', a.postal_code) as Address,a.country as Country from orders o " +
                 $"join address a on o.address_id = a.address_id; ";
-            
+
             // var orderList;
             //var transaction = trans == null ? sqlConnection.BeginTransaction() : Trans ?? trans;        
             try
@@ -77,11 +77,11 @@ namespace OrderManagement.Data
             return orderList;
         }
 
-       /* public bool CreateOrder(OrderEntity orderEntity)
-        {
-            throw new NotImplementedException();
-        }
-*/
+        /* public bool CreateOrder(OrderEntity orderEntity)
+         {
+             throw new NotImplementedException();
+         }
+ */
         public OrderEntity GetOrder(int id)
         {
             //OrderEntity order = new OrderEntity(ordEntity.OrderNumber, ordEntity.Customer_Name, ordEntity.Order_Date, ordEntity.Item, ordEntity.Price, ordEntity.Qty, ordEntity.Status, ordEntity.AddressItem);
@@ -89,7 +89,7 @@ namespace OrderManagement.Data
             //order.AddressItem = new Address().GetAddress(order.AddressId);
             GetOrders();
             var order = orderList.Find(o => o.OrderId == id);
-            return order; 
+            return order;
         }
 
         public bool CreateOrder(OrderEntity objOrdEntity)
@@ -132,7 +132,7 @@ namespace OrderManagement.Data
                 return false;
             }
 
-            
+
         }
 
 
@@ -150,9 +150,9 @@ namespace OrderManagement.Data
                 SqlCommand cmd = new SqlCommand(query, sqlConnection);
                 SqlDataReader reader = cmd.ExecuteReader();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                
+
             }
             finally
             {
@@ -161,9 +161,38 @@ namespace OrderManagement.Data
 
             GetOrders();
         }
+
+
+        public List<AddressEntity> GetAddessId()
+        {
+            string query = $"select distinct address_id from address;";
+            List<AddressEntity> list = new List<AddressEntity>();
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                // Data is accessible through the DataReader object here.
+                while (reader.Read())
+                {
+                    AddressEntity ordEntity = new AddressEntity();
+                    ordEntity.AddressId = (int)reader[0];
+                    list.Add(ordEntity);
+                }
+                reader.Close();
+                // transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                //transaction.Rollback();
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+            return list;
+        }
     }
-
-
-
-
-}
+    }
